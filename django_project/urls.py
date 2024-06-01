c934654051d43568013c0da78167ca24bc7d124d
rojasmiguel8 <rojasmiguelangel08@gmail.com>
2024-06-01 10:13:07 -0400

    signup

--- django_project/urls.py
+++ django_project/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blogs.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
     
 ]
